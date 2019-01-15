7375465e9074e7454c68c7b79a1327b16f853453
Jan <jan.pletersek@gmail.com>
2019-01-15 13:50:44 +0100

    v1.0

--- PrvaAplikacija/src/app/input-component/input-component.component.ts
+++ PrvaAplikacija/src/app/input-component/input-component.component.ts
@@ -7,20 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-component.component.css']
 })
 export class InputComponentComponent implements OnInit {
-  clickMessage: string = '1';
+  clickMessage: string = '';
   someInput: string = '';
-  poljeArray =['test1, test2'];
+  poljeArray =[];
     onClickMe() {
       this.clickMessage = this.someInput;
-      {{this.clickMessage}};
-      // let poljeObjekt = new Polje ();
-      // poljeObjekt.vnos = this.clickMessage;
-       this.poljeArray.push(this.clickMessage);
-
+      this.poljeArray.push(this.clickMessage);
     }
 
-
-  
   constructor() {}
 
   ngOnInit() {
