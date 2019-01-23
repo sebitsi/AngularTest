78e2443214d57e1909ff9189f104e363f91c7a9c
Jan <jan.pletersek@gmail.com>
2019-01-23 07:59:12 +0100

    nova aplikacija

--- PosteAplikacija/src/app/app.component.ts
+++ PosteAplikacija/src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title = 'PrvaAplikacija';
-  public data = ['Jan', 'Tomaž', 'Jure'];
-
-  public  bla (vrednost: string) {
-     console.log(vrednost);
-  }
-
+  title = 'PosteAplikacija';
 }
-
-
-
