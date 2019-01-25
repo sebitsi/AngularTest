cc4dbaf539db51e9fe8c9930293c54a3edabefcf
Jan <jan.pletersek@gmail.com>
2019-01-25 09:22:42 +0100

    avtor search deluje

--- ProjektPoste/src/app/poste/poste.component.ts
+++ ProjektPoste/src/app/poste/poste.component.ts
@@ -18,6 +18,13 @@ export class PosteComponent implements OnInit {
   searchForm: FormGroup;
   orderBy: string[] = ['Relevance', 'Newest'];
   opcije: string[] = ['All', 'Books', 'Magazines'];
+// if variables
+  titleSearch: string;
+  authorSearch: string;
+  temp1: string = null;
+  temp2: string = null;
+  temp3: string;
+  temp4: string;
   
 
   constructor(private serverService: ServerService) { }
@@ -47,11 +54,38 @@ export class PosteComponent implements OnInit {
   // }
   
   onSubmit() {
-    
+    this.titleSearch = this.searchForm.get("formtitle").value;
+    this.authorSearch = this.searchForm.get("formauthor").value;
+    // --------------------------------------------------------------------
+    if (this.titleSearch) {
+      this.temp1 = '+' + '"intitle:"'+ '+' + this.titleSearch;
+    }
+    else{
+      this.temp1 = null;
+    }
+    if (this.authorSearch) {
+      this.temp2 = '+' + '"inauthor:"' + '+' + this.authorSearch;
+    }
+    else{
+      this.temp2 = null;
+    }
+    this.temp3 = this.temp1 + this.temp2;
+    if (!this.titleSearch && !this.authorSearch){
+      this.temp4 = this.temp3;
+    }
+    else{
+      this.temp4 = this.temp3.substr(1);
+    }
+    if (this.titleSearch && !this.authorSearch){
+      this.temp4 = this.temp1.substr(1);
+    }
+    console.log(this.temp3);
+    console.log(this.temp4);
+    // --------------------------------------------------------------------
     this.params = new HttpParams().
                                   //set("q", "intitle:"+ this.searchForm.get("formtitle").value).//+"inauthor:"+'Josip').
-                                  set("q", "intitle:"+ this.searchForm.get("formtitle").value + '+' + "inauthor:"+ this.searchForm.get("formauthor").value).
-                                  //set("q", "inauthor:"+ this.searchForm.get("formauthor").value).
+                                  // set("q", "intitle:"+ this.searchForm.get("formtitle").value + '+' + "inauthor:"+ this.searchForm.get("formauthor").value).
+                                  set("q", this.temp4).
                                   set("maxResults", this.searchForm.get("formcount").value).
                                   set("orderBy", this.searchForm.get("formorder").value).
                                   set("printType", this.searchForm.get("formselect").value); 
