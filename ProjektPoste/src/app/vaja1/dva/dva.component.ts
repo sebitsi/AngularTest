e7303f67daad2b53aeb9c03a8d4437f662ec2fbc
Jan563 <47808233+Jan563@users.noreply.github.com>
2019-02-27 08:02:26 +0100

    Update dva.component.ts

--- ProjektPoste/src/app/vaja1/dva/dva.component.ts
+++ ProjektPoste/src/app/vaja1/dva/dva.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireFunctions } from '@angular/fire/functions';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-dva',
-  template: `{{ data$  | async }}`,
   styleUrls: ['./dva.component.css']
 })
 export class DvaComponent implements OnInit {
