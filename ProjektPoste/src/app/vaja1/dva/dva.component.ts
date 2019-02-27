e81d7666cdce0844ba212a054200a4b285c2026a
Jan563 <47808233+Jan563@users.noreply.github.com>
2019-02-27 08:06:35 +0100

    Update dva.component.ts

--- ProjektPoste/src/app/vaja1/dva/dva.component.ts
+++ ProjektPoste/src/app/vaja1/dva/dva.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dva',
+  templateUrl: './dva.component.html',
   styleUrls: ['./dva.component.css']
 })
 export class DvaComponent implements OnInit {
