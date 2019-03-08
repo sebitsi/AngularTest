0b8c8e4ad243b261dc7155eabc0774d60bbb8665
Jan563 <47808233+Jan563@users.noreply.github.com>
2019-03-08 10:51:14 +0100

    Update powershell.ps1

--- composetest/powershell.ps1
+++ composetest/powershell.ps1
@@ -5,6 +5,10 @@
 cd\
 
 cd \agent\_work\r2\a\_container-test\drop
+Write-Host $Env:AGENT_WORKFOLDER
+Write-Host $Env:AGENT_BUILDDIRECTORY
+Write-Host $Env:AGENT_ID
+
 
 $Env:APP_IMAGE = "$imageIdName"
 
