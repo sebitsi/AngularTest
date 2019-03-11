ce981259fa675de9af47f67f614a3fed835a9b10
Jan563 <47808233+Jan563@users.noreply.github.com>
2019-03-11 13:57:33 +0100

    Update powershell.ps1

--- composetest/powershell.ps1
+++ composetest/powershell.ps1
@@ -1,28 +1,9 @@
 param (
     [string]$imageIdName = "275",
-    [string]$artifactPath = "_containertest/drop/",
-    [string]$artifact = "nic",
-    [string]$test1 = "nic2"
+    [string]$artifactPath = "_containertest/drop/"
+
  )
  
-dir
-
-Write-Host $artifact
-Write-Host $test1
-Write-Host $Env:AGENT_WORKFOLDER
-Write-Host $Env:AGENT_BUILDDIRECTORY
-Write-Host $Env:AGENT_ID
-Write-Host $Env:BUILD_ARTIFACTSTAGINGDIRECTORY
-Write-Host $Env:BUILD_STAGINGDIRECTORY
-Write-Host $Env:SYSTEM_DEFAULTWORKINGDIRECTORY
-Write-Host $Env:BUILD_SOURCESDIRECTORY
-Write-Host $Env:BUILD_DEFINITIONNAME
-Write-Host $Env:BUILD_DEFINITIONID
-Write-Host $Env:BUILD_BUILDID
-Write-Host $Env:BUILD_BUILDNUMBER
-Write-Host $Env:BUILD_REPOSITORYNAME
-
-
 cd\
 cd $Env:SYSTEM_DEFAULTWORKINGDIRECTORY\$artifactPath
 
